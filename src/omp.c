ecfd41a4dd794555c1089cca5992bb0ebc43eb49
Beerstorm <beerstorm.emberbeard@gmail.com>
2015-10-13 00:16:38 +0200

    Kolors !!!

--- src/omp.c
+++ src/omp.c
@@ -2,6 +2,40 @@
 #include "legacy.h"
 #include "omp.h"
 
+
+void val2RGB(char * pixel, int val){
+	char r = (char) val;
+	
+	if (val < 256){
+		pixel[0] = 255 - r;
+		pixel[1] = r;
+		pixel[2] = 0;
+	}
+	else if (val < 512){
+		pixel[0] = 0;
+		pixel[1] = 255 - r;
+		pixel[2] = r;
+	}
+	else if (val < 640){
+		pixel[0] = r;
+		pixel[1] = 0;
+		pixel[2] = 255 - r;
+	}
+	else{
+		pixel[0] = 128;
+		pixel[1] = 0;
+		pixel[2] = 128;
+	}
+}
+
+
+void val2grey(char * pixel, int val){
+   pixel[0] = (char)val;
+   pixel[1] = (char)val;
+   pixel[2] = (char)val;
+}
+
+
 void omp(
     char* pixels,
     int w,
@@ -34,13 +68,11 @@ void omp(
         r = iterateOverJulia(bR, bI, cR, cI, iter);
 
         if (r >= 0) {
-            pixels[k * 3]     = (char)r;
-            pixels[k * 3 + 1] = (char)r;
-            pixels[k * 3 + 2] = (char)r;
+			val2RGB(pixels + (k * 3), r);
         } else {
-            pixels[k * 3]     = 0;
+            pixels[k * 3]     = 128;
             pixels[k * 3 + 1] = 0;
-            pixels[k * 3 + 2] = 0;
+            pixels[k * 3 + 2] = 128;
         }
     }
 }
